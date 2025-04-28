366ce754da8266ad40c8add488b9e4b46574dac7
gh-sungl <101734821+gh-sungl@users.noreply.github.com>
2025-04-28 14:40:44 +0800

    Update apool_idle_xmrmining.sh

--- apool_idle_xmrmining.sh
+++ apool_idle_xmrmining.sh
@@ -3,7 +3,7 @@
 url="http://qubic1.hk.apool.io:8001/api/qubic/epoch_challenge"
 CHECK_INTERVAL=5  # 检查间隔时间（秒）
 WORKER_NAME=$(hostname)
-MINER_CMD="/hive/miners/apoolxmr/apoolminer_hiveos/apoolminer ./xmrminer -A xmr --account CP_ejrl8u0va4 --worker my_worker --pool xmr.apool.top:3334"
+MINER_CMD="/hive/miners/apoolxmr/apoolminer_hiveos/xmrminer ./xmrminer -A xmr --account CP_ejrl8u0va4 --worker my_worker --pool xmr.apool.top:3334"
 LOG_PATH="/var/log/miner/custom/custom_cpu.log"
 
 # 创建日志文件的父目录（如果不存在）
@@ -11,8 +11,8 @@ mkdir -p "$(dirname "$LOG_PATH")"
 
 # 定义退出时清理的函数
 cleanup() {
-    echo "Stopping apoolminer and cleaning up..."
-    pkill -f apoolminer  # 结束矿工进程
+    echo "Stopping xmrminer and cleaning up..."
+    pkill -f xmrminer  # 结束矿工进程
     exit 0
 }
 
@@ -31,15 +31,15 @@ while true; do
     echo "$mining_status now, from $(date -d @$mining_time "+%Y-%m-%d %H:%M:%S")"
 
     if [ "$mining_status" == "IDLE" ]; then
-        if ! pgrep -f apoolminer > /dev/null; then
-            echo "Starting apoolminer..."
+        if ! pgrep -f xmrminer > /dev/null; then
+            echo "Starting xmrminer..."
             nohup $MINER_CMD >> "$LOG_PATH" 2>&1 &
         else
-            echo "apoolminer is already running."
+            echo "xmrminer is already running."
         fi
     elif [ "$mining_status" == "BUSY" ]; then
-        echo "Stopping apoolminer..."
-        pkill -f apoolminer
+        echo "Stopping xmrminer..."
+        pkill -f xmrminer
     fi
 
     sleep $CHECK_INTERVAL
